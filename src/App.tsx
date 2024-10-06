c01534d7243c6286bb57a247264e98c4237a5b96
Dean Reymen <deenrayman@outlook.com>
2024-10-07 01:13:52 +0700

    chore: mobile support

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,17 @@ import { ToolstackAndInspiration } from './components/ToolstackAndInspiration';
 function App() {
   return (
     <ThemeProvider>
-      <div className="relative min-h-screen flex flex-col gap-8 pt-8 pb-12 px-6 md:px-12">
+      <div className="relative min-h-screen flex flex-col gap-8 pt-8 pb-12 px-6 lg:px-12">
         <Header />
-        <main className="h-full min-h-0 grid gap-6 md:gap-8 grid-cols-2 lg:grid-cols-[3fr_3fr_1.25fr_1.25fr] md:grid-rows-[auto_auto_96px_auto] xl:grid-rows-[auto_auto_auto_auto] flex-1">
-          <Main className="lg:col-start-1 col-span-2 lg:row-start-1 row-span-2 xl:row-span-3" />
-          <Reen className="lg:col-start-1 col-span-2 lg:col-span-1 lg:row-start-3 xl:row-start-4 lg:row-span-2 xl:row-span-1" />
-          <Experience className="lg:col-start-2 col-span-2 lg:col-span-1 lg:row-start-3 xl:row-start-4 lg:row-span-2 xl:row-span-1" />
-          <References className="lg:col-start-3 col-span-2 lg:row-start-2" />
+        <main className="h-full min-h-0 grid gap-6 md:gap-8 grid-cols-2 lg:grid-cols-[4fr_4fr_1.25fr_1.25fr] lg:grid-rows-[auto_auto_auto_auto] flex-1">
+          <Main className="lg:col-start-1 col-span-2 lg:row-start-1 lg:row-span-1 xl:row-span-3" />
+          <Reen className="lg:col-start-1 col-span-2 md:col-span-1 xl:row-start-4 xl:row-span-1" />
+          <Experience className="md:col-start-2 col-span-2 md:col-span-1 xl:row-start-4 xl:row-span-1" />
+          <References className="lg:col-start-3 col-span-2 lg:row-start-2 row-span-1" />
           <ToolstackAndInspiration className="lg:col-start-3 col-span-2 lg:row-start-1" />
-          <GitHubSocial className="lg:col-start-3 col-span-1 lg:row-start-3" />
-          <LinkedInSocial className="lg:col-start-4 col-span-1 lg:row-start-3" />
-          <Contact className="lg:col-start-3 col-span-2 lg:row-start-4" />
+          <GitHubSocial className="lg:col-start-1 xl:col-start-3 col-span-1 lg:row-start-3 row-span-1" />
+          <LinkedInSocial className="lg:col-start-1 xl:col-start-4 col-span-1 lg:row-start-4 xl:row-start-3 row-span-1" />
+          <Contact className="lg:col-start-2 xl:col-start-3 col-span-2 lg:col-span-3 xl:col-span-2 xl:row-start-4 lg:row-span-2 xl:row-span-1" />
         </main>
         <Background />
       </div>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -3,8 +3,8 @@ import { Download } from 'lucide-react';
 export function Experience({ className }: { className?: string }) {
   return (
     <section className={`relative w-full p-7 flex flex-col bg-white dark:bg-black dark:bg-opacity-50 rounded-[32px] ${className}`}>
-      <h3 className="text-gray-400 dark:text-gray-300 text-base font-regular">Previous experience</h3>
-      <p className="mt-3 text-primary-500 text-xl lg:text-3xl font-medium">Frontend Developer</p>
+      <h3 className="text-gray-400 dark:text-gray-300 text-base font-regular">My experience</h3>
+      <p className="mt-3 text-primary-500 text-xl lg:text-2xl xl:text-3xl font-medium">Frontend Developer</p>
       <div className="mt-1 flex flex-row flex-wrap gap-row-2 justify-between text-gray-500 dark:text-gray-300 text-sm">
         <p className="font-regular">Verhaert Digital Innovation, 2022 — 2024</p>
       </div>
