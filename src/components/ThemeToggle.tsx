1d441f30300030d4fae2dca55ddfed2b49be7158
Dean Reymen <deenrayman@outlook.com>
2024-10-07 17:53:21 +0700

    chore: fix theme toggle accesibility

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -8,6 +8,7 @@ export function ThemeToggle({ className }: { className?: string }) {
     <button
       className={`w-[46px] h-[46px] flex flex-row py-3 px-3 items-center justify-center rounded-[32px] border-[1px] border-dashed border-gray-300 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-white dark:hover:bg-opacity-10 ${className}`}
       onClick={toggleTheme}
+      aria-label={theme === 'light' ? 'Switch to dark theme' : 'Switch to light theme'}
     >
       {theme === 'light' ? <Moon className="w-4 h-4 text-dark-900 dark:text-white" /> : <Sun className="w-4 h-4 text-gray-900 dark:text-white" />}
     </button>
