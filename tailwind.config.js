71f8c29fc676d250d00f3c67542ace81331536ce
Dean Reymen <deenrayman@outlook.com>
2024-10-07 23:32:15 +0700

    chore: background fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,31 +25,6 @@ export default {
           800: '#5a1c11',
           900: '#2d0e08'
         }
-      },
-      boxShadow: {
-        'button-inset': 'inset 0px 1px 0px 0px rgba(255, 255, 255, 0.3)'
-      },
-      keyframes: {
-        pulse: {
-          '0%': {
-            width: '0.5rem',
-            height: '0.5rem',
-            opacity: '1'
-          },
-          '50%': {
-            width: '1.5rem',
-            height: '1.5rem',
-            opacity: '0'
-          },
-          '100%': {
-            width: '1.5rem',
-            height: '1.5rem',
-            opacity: '0'
-          }
-        }
-      },
-      animation: {
-        pulse: 'pulse 2.5s infinite ease-out'
       }
     }
   },
