b58ed81ffd0eb03818a84fb8d9e6904dc23aed69
Dean Reymen <deenrayman@outlook.com>
2024-12-02 09:07:22 +0100

    chore: alignment fix

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,11 @@ function App() {
     <ThemeProvider>
       <div className="relative min-h-screen flex flex-col gap-8 pt-8 pb-12 px-4 md:px-6 lg:px-12">
         <Header />
-        <main className="flex flex-col md:grid gap-4 lg:gap-5 grid-cols-[1fr,1fr] xl:grid-cols-[2fr,1fr]">
-          <Main className="col-start-1 col-span-2 xl:col-span-1 row-start-1 xl:row-span-2" />
-          <ExperienceAndEducation className="col-start-1 xl:col-start-2 col-span-1 row-start-2 xl:row-start-1 row-span-1" />
-          <ToolstackAndInspiration className="col-start-2 col-span-1 row-start-2 row-span-1" />
-          <div className="col-start-1 col-span-2 row-start-3 grid gap-4 lg:gap-5 grid-cols-2 md:grid-cols-7 xl:grid-cols-3 grid-rows-[auto,auto,auto] xl:grid-rows-[auto,1fr]">
+        <main className="flex flex-col md:grid gap-4 lg:gap-5 grid-cols-[1fr,1fr] xl:grid-cols-[1fr,1fr,1fr]">
+          <Main className="col-start-1 col-span-2 row-start-1 xl:row-span-2" />
+          <ExperienceAndEducation className="col-start-1 xl:col-start-3 col-span-1 row-start-2 xl:row-start-1 row-span-1" />
+          <ToolstackAndInspiration className="col-start-2 xl:col-start-3 col-span-1 row-start-2 row-span-1" />
+          <div className="col-start-1 col-span-2 xl:col-span-3 row-start-3 grid gap-4 lg:gap-5 grid-cols-2 md:grid-cols-7 xl:grid-cols-3 grid-rows-[auto,auto,auto] xl:grid-rows-[auto,1fr]">
             <ProjectList className="col-start-1 col-span-2 md:col-span-5 xl:col-span-1 row-start-1 row-span-1 md:row-span-2" />
             <References className="col-start-1 xl:col-start-2 col-span-2 md:col-span-4 xl:col-span-1 row-start-2 md:row-start-3 xl:row-start-1 row-span-1" />
             <Contact className="col-start-1 md:col-start-5 xl:col-start-3 col-span-2 md:col-span-3 xl:col-span-1 row-start-4 md:row-start-3 xl:row-start-1 row-span-1" />
